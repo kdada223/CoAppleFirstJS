4703e5f1e5022a1c1d23e50657e39645562602a8
CoCookNam <58898852+kdada223@users.noreply.github.com>
2025-11-28 14:53:57 +0900

    feat: Remove problem statements from event-ajax,map,filter,sort.js
    Removed problem statements and hints related to filtering, sorting, and localStorage functionality.

--- playground/event-ajax,map,filter,sort.js
+++ playground/event-ajax,map,filter,sort.js
@@ -23,8 +23,6 @@ function renderCards(arr) {
 
 // [문제1] filter/map/sort를 이용하여 products 배열에서
 // (1) "가격이 60000원 이상"인 객체만 뽑아 새 배열(filtered) 생성
-// (2) "상품명을 전부 [한글로] 뒤에 '(특가)' 붙여서 출력"하는 배열(titleEvents) 생성
-// (3) 가격 기준 내림차순 정렬하여(원본보존) cards로 그리기
 
 // [문제2] for문 또는 forEach로 상품 가격의 총합을 구하세요
 // (출력 예: "총 가격: 180000")
@@ -37,14 +35,4 @@ function renderCards(arr) {
 // - 이미 2번 모두 데이터를 받아오면 버튼 사라지게!
 // - 필요한 html: <button id="getMore">더보기</button>
 
-// [문제4] 상품명(title)에 특정 단어(예: 'Shirt')가 포함된 상품만 카드로 그려보기
-// (힌트: includes 활용, renderCards 함수 재사용!)
 
-// [문제5] localStorage를 사용하여 마지막으로 렌더링했던 상품명을 저장/로딩 기능 추가하기
-// ex) 카드를 그릴 때마다 마지막 상품명(localStorage.setItem), 
-//     페이지 새로고침 시엔 그 값을 콘솔에 자동 출력
-
-/* [실행에 필요한 참고 코드/준비물]
-products 배열, renderCards(), .row div, #getMore 버튼(문제3)
-반드시 변수/함수/HTML 주석 참고해서 문제 풀이할 것!
-*/
